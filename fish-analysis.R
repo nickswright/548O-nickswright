a891207241b685ee3a1fa13e1f897c4d47a5313d
Nicholas Wright <nickswright98@gmail.com>
2023-11-09 14:33:44 -0800

    Deleted the first mutate function and replaced it with a new one with different size categories. Added a comment describing what this code does.

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,8 +1,9 @@
+#This code takes the fish data cv and then creates a new data frame named fish_data_cat
+#in which the length_cat column is mutated into big (length>300) and small (length<300) categories.
 fish_data = read.csv("Gaeta_etal_CLC_data.csv")
-
-library(dplyr)
-fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
 
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
+
