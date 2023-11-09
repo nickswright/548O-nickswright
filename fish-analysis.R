6ff59a5acc9db86349efae5cd25c1b7a48122696
Nicholas Wright <nickswright98@gmail.com>
2023-11-09 14:26:38 -0800

    Change size cutoff for new column

--- fish-analysis.R
+++ fish-analysis.R
@@ -1,4 +1,8 @@
 fish_data = read.csv("Gaeta_etal_CLC_data.csv")
+
 library(dplyr)
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
