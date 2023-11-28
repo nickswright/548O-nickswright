61aa63faf3ac4f4cb5d693178edcdca8aa2b2246
Nicholas Wright <nickswright98@gmail.com>
2023-11-28 14:09:20 -0800

    Exercise 1

---
+++ Regex.R
@@ -0,0 +1,39 @@
+library(stringr)
+gDat <- gapminder::gapminder
+str(gDat)
+library(tidyverse)
+
+
+
+#Quantifiers
+(strings <- c("a", "ab", "acb", "accb", "acccb", "accccb"))
+grep("ac*b", strings, value = TRUE)
+grep("ac+b", strings, value = TRUE)
+grep("ac?b", strings, value = TRUE)
+grep("ac{2}b", strings, value = TRUE)
+grep("ac{2,}b", strings, value = TRUE)
+grep("ac{2,3}b", strings, value = TRUE)
+
+#Exercise 1
+grep("e{2,}", gDat$country, value = TRUE)
+
+#Position of pattern within the string
+(strings <- c("abcd", "cdab", "cabd", "c abd"))
+grep("ab", strings, value = TRUE)
+grep("^ab", strings, value = TRUE)
+grep("ab$", strings, value = TRUE)
+grep("\\bab", strings, value = TRUE)
+
+#Operators
+(strings <- c("^ab", "ab", "abc", "abd", "abe", "ab 12"))
+grep("ab.", strings, value = TRUE)
+grep("ab[c-e]", strings, value = TRUE)
+grep("ab[^c]", strings, value = TRUE)
+grep("^ab", strings, value = TRUE)
+grep("\\^ab", strings, value = TRUE)
+grep("abc|abd", strings, value = TRUE)
+gsub("(ab) 12", "\\1 34", strings)
+
+#Exercise 2
+
+
